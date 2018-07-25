8a219a9cf2cb637695dc90ea219aa82a9cb3f084
Thomas Higginbotham <thomas@thomashigginbotham.com>
2018-07-25 13:34:26 -0400

    Add Start page.

---
+++ EpiMenuTutorial/Models/Pages/StartPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiMenuTutorial.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "cd0ca9f5-b3c5-4ac5-b560-b586647dc552", Description = "")]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
